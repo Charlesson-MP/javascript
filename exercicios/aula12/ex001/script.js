9707f4555180f9415c95c752c7088e5a1eff2ecd
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2024-07-06 09:10:06 -0300

    Imprementando o script do exercício 1

--- exercicios/aula12/ex001/script.js
+++ exercicios/aula12/ex001/script.js
@@ -1,1 +1,9 @@
 var msg = window.document.getElementById('msg')
+var corpo = window.document.querySelector('body')
+var agora = new Date()
+var hora = agora.getHours()
+var minuto = agora.getMinutes()
+
+if(hora < 12) {
+    msg.innerHTML = '<p></p>'
+}
